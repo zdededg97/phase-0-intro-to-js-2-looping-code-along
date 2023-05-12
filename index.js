5398e6acead504b614323fa36fd8a664892caec7
Edgar Hernandez <zdededg@gmail.com>
2023-05-12 13:43:24 -0700

    solved

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+  const thankYouMessages = [];
+  
+  for (let i = 0; i < namesArray.length; i++) {
+    thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+  }
+  
+  return thankYouMessages;
+}
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
